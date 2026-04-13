5266c16c8e68b4136ad8163f2b17eec8909dccca
ross0778 <ross0778@umn.edu>
2026-04-12 23:09:27 -0500

    Comments deleted

--- msi/week11.sh
+++ msi/week11.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
-# this says that this is a shell script
 #SBATCH --nodes=1
-# this specifies one node
 #SBATCH --ntasks=16
-# this specifies that the process should run 8 tasks
 #SBATCH --mem=32gb
-# this saves 32 gigabytes of RAM for the process
 #SBATCH -t 2:00:00
 #SBATCH --mail-user=ross0778@umn.edu
-# specifies my email
 #SBATCH --mail-type=ALL
 #SBATCH -p msismall
-# this specifies which partition to run on, which in this case is msismall
 
 cd ~/psy8712-week11/msi
 module load R/4.4.2-openblas-rocky8
