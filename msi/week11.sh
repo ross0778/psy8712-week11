b4b50546c1bc32d8c1761ec4cdbb8767b67f56b4
ross0778 <ross0778@umn.edu>
2026-04-12 20:57:28 -0500

    Update script

--- msi/week11.sh
+++ msi/week11.sh
@@ -6,15 +6,15 @@
 # this specifies that the process should run 8 tasks
 #SBATCH --mem=32gb
 # this saves 32 gigabytes of RAM for the process
-#SBATCH --p msismall
+#SBATCH -p msismall
 # this specifies which partition to run on, which in this case is msismall
 #SBATCH -o week11.out
 # this specifies to save everything to week11.out
 #SBATCH --mail-user=ross0778@umn.edu
 # specifies my email
 #SBATCH --mail-type=ALL
+#SBATCH -t 2:00:00
 
 cd ~/psy8712-week11/msi
-mkdir -p ../out
 module load R/4.4.2-openblas-rocky8
 Rscript week11-cluster.R
