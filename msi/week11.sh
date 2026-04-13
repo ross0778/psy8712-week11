bbbc3d351e7df1b81e046aa137b22595d120f23c
ross0778 <ross0778@umn.edu>
2026-04-12 21:53:20 -0500

    Change task orders

--- msi/week11.sh
+++ msi/week11.sh
@@ -2,18 +2,16 @@
 # this says that this is a shell script
 #SBATCH --nodes=1
 # this specifies one node
-#SBATCH --ntasks=8
+#SBATCH --ntasks=16
 # this specifies that the process should run 8 tasks
 #SBATCH --mem=32gb
 # this saves 32 gigabytes of RAM for the process
-#SBATCH -p msismall
-# this specifies which partition to run on, which in this case is msismall
-#SBATCH -o week11.out
-# this specifies to save everything to week11.out
+#SBATCH -t 2:00:00
 #SBATCH --mail-user=ross0778@umn.edu
 # specifies my email
 #SBATCH --mail-type=ALL
-#SBATCH -t 2:00:00
+#SBATCH -p msismall
+# this specifies which partition to run on, which in this case is msismall
 
 cd ~/psy8712-week11/msi
 module load R/4.4.2-openblas-rocky8
